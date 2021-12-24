2e3e22eb6370ea498403d2f795fe53b985d6ebaa
YoungXinyu1802 <yxy1802@163.com>
2021-12-24 11:41:57 +0800

    update createTask
    修改了创建任务的选择图片

--- frontend/src/views/CreateTask.vue
+++ frontend/src/views/CreateTask.vue
@@ -20,33 +20,6 @@
         >
         </el-input>
       </el-form-item>
-<!--      <el-form-item label="标注标签">-->
-<!--        <el-tag-->
-<!--          :key="tag"-->
-<!--          v-for="tag in task.dynamicTags"-->
-<!--          closable-->
-<!--          :disable-transitions="false"-->
-<!--          @close="handleClose(tag)">-->
-<!--          {{tag}}-->
-<!--        </el-tag>-->
-<!--        <el-input-->
-<!--          class="input-new-tag"-->
-<!--          v-if="inputVisible"-->
-<!--          v-model="inputValue"-->
-<!--          ref="saveTagInput"-->
-<!--          size="small"-->
-<!--          @keyup.enter.native="handleInputConfirm"-->
-<!--          @blur="handleInputConfirm"-->
-<!--        >-->
-<!--        </el-input>-->
-<!--        <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>-->
-<!--      </el-form-item>-->
-<!--      <el-form-item label="文件类型">-->
-<!--        <el-radio-group v-model="task.resource">-->
-<!--          <el-radio label="图片"></el-radio>-->
-<!--          <el-radio label="视频"></el-radio>-->
-<!--        </el-radio-group>-->
-<!--      </el-form-item>-->
       <el-form-item label="任务描述">
         <el-input
           type="textarea"
@@ -55,6 +28,9 @@
         >
         </el-input>
       </el-form-item>
+      <el-form-item label = "任务图片">
+        <el-button type="text" @click="dialogTableVisible = true">请选择图片</el-button>
+      </el-form-item>
 
       <el-form-item>
         <el-button type="primary" @click="onSubmit">立即创建</el-button>
@@ -62,57 +38,22 @@
       </el-form-item>
     </el-form>
     </el-card>
-
-    <el-card shadow="always">
-      <div slot="header" class="el-card-header">
-        <h2 class="login-title">上传图片</h2>
-      </div>
-      <el-upload
-        name = 'file'
-        ref="upload"
-        action="http://127.0.0.1:8000/api/upload"
-        :data="{username:this.username}"
-        list-type="picture-card"
-        :before-upload="beforeAvatarUpload"
-        multiple
-        :auto-upload="false"
+    <el-dialog title="选择图片" :visible.sync="dialogTableVisible">
+      <el-checkbox-group
+        v-model="checkList"
       >
-          <i slot="default" class="el-icon-plus"></i>
-          <div slot="file" slot-scope="{file}">
-            <img
-              class="el-upload-list__item-thumbnail"
-              :src="file.url" alt=""
-            >
-            <span class="el-upload-list__item-actions">
-              <span
-                class="el-upload-list__item-preview"
-                @click="handlePictureCardPreview(file)"
-              >
-                <i class="el-icon-zoom-in"></i>
-              </span>
-              <span
-                v-if="!disabled"
-                class="el-upload-list__item-delete"
-                @click="handleDownload(file)"
-              >
-                <i class="el-icon-download"></i>
-              </span>
-              <span
-                v-if="!disabled"
-                class="el-upload-list__item-delete"
-                @click="handleRemove(file)"
-              >
-                <i class="el-icon-delete"></i>
-              </span>
-            </span>
-          </div>
-      </el-upload>
-      <el-dialog :visible.sync="dialogVisible">
-        <img width="100%" :src="dialogImageUrl" alt="">
-      </el-dialog>
+        <el-checkbox v-for="v in imglist" :label="v.filename" :key="v.filename">
+          {{v.filename}}
+          <div> <img :src = "v.url" alt="" class="img"></div>
+        </el-checkbox>
+      </el-checkbox-group>
 
-    </el-card>
-    <el-button type="primary" class="el-button" @click="submitUpload">创建</el-button>
+
+      <el-button type="primary" @click="selectImg">完成</el-button>
+
+
+    </el-dialog>
+
   </div>
 </template>
 
@@ -127,6 +68,16 @@ export default {
   components: { Hints },
     data() {
       return {
+        imglist: [
+          { filename: 'hello',
+            url: 'https://seopic.699pic.com/photo/50050/5027.jpg_wh1200.jpg'
+          },
+          { filename: 'yes',
+            url: 'https://seopic.699pic.com/photo/50050/5027.jpg_wh1200.jpg'
+          }
+        ],
+        checkList: [],
+        multipleSelection: [],
         username: localStorage.getItem('username'),
         task: {
           name: '',
@@ -141,11 +92,32 @@ export default {
 
         dialogImageUrl: '',
         dialogVisible: false,
-        disabled: false,
 
-      }
+        dialogTableVisible: false,
+        dialogFormVisible: false,
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '120px'
+      };
+
+
+
     },
     methods: {
+      selectImg(){
+        console.log(this.checkList)
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val
+      },
       onSubmit() {
         console.log('submit!');
         console.log(this.username)
@@ -262,6 +234,10 @@ export default {
   .el-button{
     position: relative;
     margin-top: 10px
+  }
+  .img{
+    width: 100px;
+    height: 100px;
   }
 
   //.form-list {
