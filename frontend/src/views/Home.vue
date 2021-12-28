90af539b5169bad013a8681d2415ef8fdc9759dd
YoungXinyu1802 <yxy1802@163.com>
2021-12-28 19:47:54 +0800

    update Home
    修改首页

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="home-wrapper">
-    <el-card shadow="always" :body-style="{padding: '0px'}">
-        <img src="../assets/img/AnnoLogo.png" alt="icon" class="img">
-        <h3 class="title">欢迎来到数据集标注网站</h3>
-<!--          <ChartsLine :title="title" :data="commonChartsData" class="data-chart" />-->
+    <el-card shadow="always" :body-style="{padding: '0px'}" class="card">
+        <img src="../assets/img/Home.png" alt="icon" class="img">
     </el-card>
 
     <div class="date-row">
@@ -84,38 +82,6 @@ export default {
   components: { CountTo, ChartsPie, ChartsBar, ChartsLine },
   data() {
     return {
-      text: '用户访问来源',
-      title: '近七天用户访问量条形图',
-      langsData: [
-        { name: 'VUE', value: 45, color: '#2d8cf0' },
-        { name: 'ES6', value: 32, color: '#19be6b' },
-        { name: 'TS', value: 4, color: '#ff9900' },
-        { name: 'LESS', value: 13, color: '#e46cbb' },
-        { name: 'HTML', value: 6, color: '#9a66e4' }
-      ],
-      cardInfoData: [
-        { title: '今日点击', icon: 'vue-dsn-icon-dianji', count: 682, color: '#2d8cf0' },
-        { title: '新增用户', icon: 'vue-dsn-icon-xinzeng', count: 259, color: '#19be6b' },
-        { title: '信息发送', icon: 'vue-dsn-icon-xinfeng', count: 1262, color: '#ff9900' },
-        { title: '点赞统计', icon: 'vue-dsn-icon-dianzan', count: 508, color: '#e46cbb' },
-        { title: '累计收藏', icon: 'vue-dsn-icon-heart', count: 379, color: '#9a66e4' }
-      ],
-      chartsPieData: [
-        { value: 1920, name: '直接访问' },
-        { value: 1169, name: '邮件营销' },
-        { value: 986, name: '联盟广告' },
-        { value: 621, name: '视频广告' },
-        { value: 3256, name: '搜索引擎' }
-      ],
-      commonChartsData: {
-        Mon: 782,
-        Tue: 925,
-        Wed: 1196,
-        Thu: 812,
-        Fri: 328,
-        Sat: 222,
-        Sun: 1080
-      }
     }
   },
   methods: {
@@ -139,20 +105,14 @@ export default {
 </script>
 
 <style lang="less">
-//@import "../assets/less/home";
-//.img{
-//  width: 80px;
-//  height: 80px
-//}
 .home-wrapper{
   .img{
-    height: 300px;
-    width: 300px;
-    margin-left: 500px;
-    margin-top: 100px
+    height: 200px;
+    display: flex;
+    margin-top: 50px
   }
   .date-row{
-    margin-top: 10px;
+    margin-top: 30px;
     width: 100%;
     display: flex;
     margin-bottom: 20px;
@@ -213,6 +173,9 @@ export default {
       height: 48px;
       line-height: 20px;
     }
+  }
+  .card{
+    height: 350px;
   }
 }
 </style>
